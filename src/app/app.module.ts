aff90ce795d31cb5f19b9a7a589144781d6b466b
alfred-esmond-cap <alfredo.esteban-mondria.external@capgemini.com>
2024-10-29 15:07:54 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SharedModule } from './shared/shared.module';
+
+// Configuración del locale de la app
+import localeEsES from '@angular/common/locales/es';
+import localeFrCA from '@angular/common/locales/fr-CA';
+
+import { registerLocaleData } from '@angular/common';
+
+
+registerLocaleData( localeEsES );
+registerLocaleData( localeFrCA );
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    SharedModule,
+  ],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es' }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -0,0 +1,60 @@
+import { Component } from '@angular/core';
+import { interval, Observable, tap } from 'rxjs';
+
+@Component({
+  selector: 'app-uncommon-page',
+  templateUrl: './uncommon-page.component.html',
+  styleUrls: ['./uncommon-page.component.css']
+})
+export class UncommonPageComponent {
+
+  // i18n Select
+  public name: string = 'Alfredo';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient():void {
+    this.name = 'Marta';
+    this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Maria','Pedro','Alfredo', 'Hernando', 'Eduardo', 'Melissa', 'Natalia'];
+  public clientsMap = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # clientes esperando.',
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Alfredo',
+    age: 33,
+    address: 'Valencia, España',
+  }
+
+  // Async Pipe
+  public myObservableTimer: Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:', value ) ),
+  );
+
+  public promiseValue: Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve( 'Tenemos data en la promesa.' );
+      console.log( 'Tenemos data en la promesa.' );
+      this.person.name = 'Otro nombre'
+    }, 3500);
+  })
+
+
+
+
+}
